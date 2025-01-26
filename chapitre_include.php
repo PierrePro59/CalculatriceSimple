c2e2b4ae3714089fbb9648d1b3f8282cfc52a021
PierrePro59 <pierre.vasseur0406@gmail.com>
2025-01-26 18:57:40 +0100

    version initiale du projet

---
+++ chapitre_include.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html> 
+	<head>
+		<meta charset="utf-8" />
+		<title>Apprendre le include</title> 
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+<body>
+
+<div id="container">
+<header>
+	Ici mon header
+</header> <!-- fermeture header -->
+
+<nav>
+	<ul>
+		<li>Home</li>
+		<li>Rubrique 1</li>
+		<li>Rubrique 2</li>
+		<li>Rubrique 3</li>
+	</ul>
+</nav> <!-- fermeture navbar -->
+
+<div id="contenu_1">
+	Ici mon bloc 1 principal 1
+	Ce site vous présentera une branche
+</div> <!-- fermeture contenu_1 -->
+
+<div id="contenu_2">
+	Ici mon bloc 2 principal 2
+	correction sur une branche
+</div> <!-- fermeture contenu_2 -->
+
+<div id="clear"></div>
+<footer>
+	Ici mon footer
+</footer> <!-- fermeture footer -->
+</div> <!-- fermeture container -->
+</body>
+
+</html>
